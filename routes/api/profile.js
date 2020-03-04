42b5735923d6d4ae963c3218e3f4d27440f62c3f
MdYusufBinNur <engr.mdyusuf@gmail.com>
2020-03-04 18:03:59 +0600

    Init msg

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const auth = require('../../middleware/auth');
 const router = express.Router();
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
-//@route GET api/profile
-//@desc  Test route
-//@access  Public
-router.get('/',(req,res) => res.send('Profile Route'));
+//@route GET api/profile/me
+//@desc  Get Current user Profile
+//@access  private
+router.get('/me', auth, async (req,res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user',['name','avatar']);
+        if (!profile){
+            return res.status(400).json({ msg: "There is no profile for this user"})
+        }
+        await res.json(profile)
+    }catch (err) {
+        console.error(err.message);
+        res.status(500).send('SERVER ERROR');
+    }
+});
 
 module.exports = router;
--- routes/api/user.js
+++ routes/api/user.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const gravator = require('gravator');
+const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config')
 const { check, validationResult } = require('express-validator');
-//Import Models
+//Import models
 const User = require('../../models/User');
 
 //@route GET api/user
@@ -25,18 +27,43 @@ router.post(
         const { name, email, password} = req.body;
         try {
             //See if user already exist
-            let user = User.findOne({ email });
+            let user = await User.findOne({ email });
             if (user){
-                 res.status(400).json({ errors: [{ msg: 'User Already Exist' }]});
+                return  res.status(400).json({ errors: [{ msg: 'User Already Exist' }]});
             }
             //Get user gravator
 
+            const avatar = gravatar.url(email,{
+                s: '200',
+                r: 'pg',
+                d: 'mm'
+            });
+            user = new User({ name, email, avatar,password});
+
             //Encrypted password
+            const  salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(password, salt);
+
+            //Save the user
+            await user.save();
+
+            const payload ={
+                user: {
+                    id: user.id
+                }
+            };
+            jwt.sign(payload,
+                config.get('jwtSecret'),
+                { expiresIn: 360000 },
+                (err, token) => {
+                if (err) throw err;
+                    res.json({ token })
+                });
             //Return jsonwebtoken
-            res.send('User Route')
+            //res.send('User Registered')
         }catch (err) {
             console.error(err.message);
-            res.status(500).send('Server Error');
+             res.status(500).send('Server Error');
         }
 
     });
